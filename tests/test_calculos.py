e8e52937330214ff00f0e37ac9e099adc79a8dc4
Felipe Wilson Viana <2403814@SALAS.AULAS>
2026-09-14 11:41:00 -0300

    update ap1

--- tests/test_calculos.py
+++ tests/test_calculos.py
@@ -4,6 +4,6 @@ def test_calcular_valor_sem_desconto(pedido_simples):
 
     assert calcular_total_com_desconto(pedido_simples) == 45.00
 
-def calcular_valor_com_desconto(pedido_premium):
+def test_calcular_valor_com_desconto(pedido_premium):
 
     assert calcular_total_com_desconto(pedido_premium) == 108.00
